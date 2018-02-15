cc1234145fbaadc436c8d0872655b20780c5ca36
Neel Banerjee <neel.banerjee@urbanairship.com>
2018-02-14 22:40:16 -0800

    Update recordCam.sh

--- recordCam.sh
+++ recordCam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # recordCam.sh
-# The file name. I use the date to make finding files easier.
-url='username:passsword@yourdomain.ddns.net:554/11'
-BASEpath='/home/ubuntu'
+url='username:password@yourdomain.ddns.net:554/11'
+BASEpath='/home/ubuntu/videos'
+mkdir -p $BASEpath
 
 avconv -rtsp_transport tcp -i rtsp://${url} \
 -c:v copy -an -map 0 -f segment -segment_time 300 -segment_format mp4 -strict -2 \
